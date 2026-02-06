471a6119f25f3aa9b3bd721e98b92cc8858a9220
unknown <asim26k@gmail.com>
2026-02-06 06:50:33 +0530

    ...

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,3 +1,7 @@
+import mongoose from mongoose;
+import User from "../models/User.js";
+import bcrypt from "bcryptjs"
+
 export const signup = async (req, res) => {
   console.log("This is sign up controller.");
   res.send("Welcome to signup route.");
