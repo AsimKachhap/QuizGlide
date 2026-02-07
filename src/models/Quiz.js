4e5081f028f3e742467ecff36b80f36f5e3c8d58
unknown <asim26k@gmail.com>
2026-02-07 18:29:31 +0530

    Create Quiz model

---
+++ src/models/Quiz.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+import User from "./User";
+
+const quizSchema = mongoose.Schema(
+  {
+    owner: {
+      type: mongoose.Types.ObjectId,
+      ref: User,
+      required: true,
+    },
+
+    isComplete: {
+      type: Boolean,
+      default: false,
+    },
+
+    questions: [
+      {
+        question: {
+          type: String,
+          required: true,
+        },
+
+        opt1: {
+          type: String,
+          required: true,
+          isCorrect: false,
+        },
+        opt2: {
+          type: String,
+          required: true,
+          isCorrect: false,
+        },
+        opt3: {
+          type: String,
+          required: true,
+          isCorrect: false,
+        },
+        opt4: {
+          type: String,
+          required: true,
+          isCorrect: false,
+        },
+      },
+    ],
+  },
+  {
+    timestapms: true,
+  },
+);
+
+const Quiz = mongoose.model("Quiz", quizSchema);
+export default Quiz;
